b2137881aafa466e97cad8c30280140d14f51324
Aleksandr912 <154664349+Aleksandr912@users.noreply.github.com>
2023-12-24 10:30:04 +0500

    Add files via upload

---
+++ SEM_1/2_12/2_12.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int max=-999999;
+	int min=99999;
+	int sum;
+	for (int i = 0; i < n; i++){
+		int a;
+		cin >> a;
+		if (a >= max) {
+			max = a;
+		}
+		if (a <= min) {
+			min = a;
+		}
+	}
+	sum = min + max;
+	cout << sum;
+	return 0;
+}
+
