4995d0af89e558106df9962e1d670028ef01fb8d
Aleksandr912 <154664349+Aleksandr912@users.noreply.github.com>
2023-12-24 11:35:54 +0500

    Add files via upload

---
+++ SEM_1/2_17/2_17.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int x;
+    cin >> x;
+    float sum = 1;
+    for (int i = 1; i < n; i++) {
+        int y = 1;
+        for (int a = 1; a < i; a++) {
+            y = y * (y + 1);
+        }
+        sum = sum + pow(x, i) / (y);
+    }
+    cout << sum;
+    return 0;
+}
