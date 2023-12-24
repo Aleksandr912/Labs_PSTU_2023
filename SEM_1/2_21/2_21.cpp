09863904feb161bf8899264b60df3425cc6a3006
Aleksandr912 <154664349+Aleksandr912@users.noreply.github.com>
2023-12-24 11:37:23 +0500

    Add files via upload

---
+++ SEM_1/2_21/2_21.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n=-10000000;
+    int c;
+    bool a = true;
+    bool b = true;
+    while (b==true) {
+        cin >> c;
+        if (c >= n) {
+            n = c;
+        }
+        if ((c < n) and (c!=0)) {
+            a = false;
+        }
+        if (c == 0) {
+            b = false;
+        }
+    }
+    if (a == true) {
+        cout << " po vozrastaniu";
+    }
+    if (a == false) {
+        cout << "ne po vozrastaniu";
+    }
+    return 0;
+}
+
