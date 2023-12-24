ca866e4942ff4bc34c28442155318b98febe7db3
Aleksandr912 <154664349+Aleksandr912@users.noreply.github.com>
2023-12-24 10:27:25 +0500

    Add files via upload

---
+++ SEM_1/2_6/2_6.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int stars = n;
+	int spaces = n;
+	for (int i = 1; i <= n; i++){
+		for (int j = n; j <= spaces; j++){
+			cout << " ";
+		}
+		spaces++;
+		for (int j = 0; j < stars; j++){
+			cout << "*";
+		}
+		stars -= 1;
+		cout << endl;
+	}
+	return 0;
+}
