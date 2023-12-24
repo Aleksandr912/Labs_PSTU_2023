3d9dae5e5a7211f30ceb694104f348f15f85b512
Aleksandr912 <154664349+Aleksandr912@users.noreply.github.com>
2023-12-24 11:37:45 +0500

    Add files via upload

---
+++ SEM_1/2_22/2_22.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+#include <cmath>;
+
+int main()
+{
+    int n;
+    cin >> n;
+    float max = -99999999.9;
+    float a;
+    int b;
+    for (int i = 1; i <= n; i++) {
+        a = sin(n + (i / n));
+        if (a > max) {
+            max = a;
+            b = i;
+        }
+    }
+    cout << max << endl;
+    cout << b;
+    return 0;
+}
+
+
