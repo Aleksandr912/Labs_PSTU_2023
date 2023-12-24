93463bfd9563edd7d1a3931444d3e0633ecee996
Aleksandr912 <154664349+Aleksandr912@users.noreply.github.com>
2023-12-24 11:48:09 +0500

    Add files via upload

---
+++ SEM_1/Ptr/Ptr_8.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string c, a, b;
+	cin >> c >> a >> b;
+	int z = 0;
+	int x = 0;
+	int k = 0;
+	int l = c.length();
+	for (int i = 0; i < l; i++) {
+		string sim = c.substr(i, 1);
+		if (sim == a)
+			z++;
+		else if (sim == b)
+			x++;
+		else
+			k++;
+	}
+	cout << z << endl;
+	cout << x << endl; 
+	cout << k << endl;
+	cout << c.length();
+	return 0;
+}
