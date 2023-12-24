00323bcffbc7840efa4bc81ac390a91c3f721651
Aleksandr912 <154664349+Aleksandr912@users.noreply.github.com>
2023-12-24 11:35:07 +0500

    Add files via upload

---
+++ SEM_1/2_15/2_15.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru_RU");
+	int n , s;
+	bool k = 0;
+	cin >> n;
+	cin >> s;
+	while (n != 0)
+	{
+		if (n % 10 == s) {
+			k = 1;
+		}
+		n = n / 10;
+	}
+	if (k == 1) {
+		cout << "входит число " << s;
+	}
+	if (k == 0) {
+		cout << "нет таких чисел";
+	}
+	return 0;
+ 
+}
+
