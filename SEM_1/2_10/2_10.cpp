bccb5977e628e3d1a2e298425a8f5d360a781e91
Aleksandr912 <154664349+Aleksandr912@users.noreply.github.com>
2023-12-24 10:29:09 +0500

    Add files via upload

---
+++ SEM_1/2_10/2_10.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n, a;
+	cin >> n >> a;
+	int max = a;
+	for (int i = 2; i <= n; i++){
+		int b;
+		cin >> b;
+		if (b > max){
+			max = b;
+		}
+	}
+	cout << max;
+	return 0;
+}
