aabf78253c3f0131970ea48b4f951282dd14f04e
Aleksandr912 <154664349+Aleksandr912@users.noreply.github.com>
2023-12-24 10:28:26 +0500

    Add files via upload

---
+++ SEM_1/2_8/2_8.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	if (n < 2){
+		cout << "NOT CORRECT";
+	}
+	else{
+		for (int i = 1; i <= n; i++){
+			for (int j = 1; j <= i; j++){
+				cout << "* ";
+			}
+			cout << endl;
+		}
+	}
+	return 0;
+}
