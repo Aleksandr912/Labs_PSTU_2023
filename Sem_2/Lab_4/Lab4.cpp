4e22581fc45715f2a3b68a7c7267c76dcaac353a
Aleksandr912 <154664349+Aleksandr912@users.noreply.github.com>
2024-02-21 16:05:42 +0500

    Add files via upload

---
+++ Sem_2/Lab_4/Lab4.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a[100];
+    int k;
+    cin >> k;
+    for (int i = 0; i < 10; i++) {
+        a[i] = rand();
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < 10; i++) {
+        a[i] = a[i + 1];
+    }
+    for (int i = 0; i < 9; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    cout << "______________________" << endl;
+    int n;
+    for (int i = 0; i < k; i++) {
+        n = a[8];
+        for (int j = 8; j > 0; j--) {
+            a[j] = a[j - 1];
+        }
+        a[0] = n;
+        for (int t = 0; t < 9; t++) {
+            cout << a[t] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
