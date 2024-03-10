8eaa793f0b1c70e5d9bca2932d1ec336fa1904ef
Aleksandr912 <154664349+Aleksandr912@users.noreply.github.com>
2024-03-10 11:12:35 +0500

    Add files via upload

---
+++ Sem_2/Lab_7/Lab_7.2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+
+int kvadr(int m, ...) 
+{
+	int* p = &m + 1;
+	int k = 0;
+	int kor;
+	for (; m != 0; m--)
+	{
+		kor = sqrt(*p);
+		if (kor * kor == *p) k++; 
+		p++; 
+	}
+	return k;
+}
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "ru_RU");
+	int n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11;
+	int ch;
+	cout << "Сколько чисел вы хотите проверить(3,7 или 11)? ";
+	cin >> ch; 
+	if (ch == 3)
+	{
+		cout << ("Введите 3 числа: ");
+		cin >> n1 >> n2 >> n3;
+		cout << "Количество точных квадратов: " << kvadr(3, n1, n2, n3);
+	}
+
+	if (ch == 7)
+	{
+		cout << ("Введите 7 чисел: ");
+		cin >> n1 >> n2 >> n3 >> n4 >> n5 >> n6 >> n7;
+		cout << ("Количество точных квадратов: ") << kvadr(7, n1, n2, n3, n4, n5, n6, n7);
+	}
+	if (ch == 11)
+	{
+		cout << ("Введите 11 чисел: ");
+		cin >> n1 >> n2 >> n3 >> n4 >> n5 >> n6 >> n7 >> n8 >> n9 >> n10 >> n11;
+		cout << ("Количество точных квадратов: ") << kvadr(11, n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11);
+	}
+	return 0;
+}
---
+++ Sem_2/Lab_7/lab_7.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+float diff(float a, float b) {
+	return a - b;
+}
+
+double diff(double a, double b) {
+	return a - b;
+}
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	float a;
+	float b;
+	cout << "Введите первое число" << endl;
+	cin >> a;
+	cout << "Введите второе число" << endl;
+	cin >> b;
+	cout << "Разность:" << diff(a, b) << endl;
+	return 0;
+}
